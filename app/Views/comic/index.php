936b05d3c94daaef5e8a7ebf40de23006526c946
GusArjuna <taufanmaruf26@gmail.com>
2020-08-05 12:58:59 +0700

    adding style.css

--- app/Views/comic/index.php
+++ app/Views/comic/index.php
@@ -5,28 +5,30 @@
     <div class="row">
         <div class="col">
             <h3 class="mt-2">List Comics</h3>
-            <table class="table table-striped table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Pic</th>
-                        <th scope="col">Title</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td><img src="/img/kanokari.JPG" alt="" width="70"></td>
-                        <td>Narto</td>
-                        <td>
-                            <a href="" class="btn btn-primary">Detail</a>
-                            <a href="" class="btn btn-success">Edit</a>
-                            <a href="" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="comics-table">
+                <table class="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Pic</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td><img src="/img/kanokari.JPG" alt="" width="70"></td>
+                            <td>Narto</td>
+                            <td>
+                                <a href="" class="btn btn-primary">Detail</a>
+                                <a href="" class="btn btn-success">Edit</a>
+                                <a href="" class="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
